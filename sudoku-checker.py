e441fceae9bb425012fe5d58d815a9526b2c2744
Mittal Makwana <mittalmak@gmail.com>
2018-11-10 23:53:37 -0800

    Added sudoku class

--- sudoku-checker.py
+++ sudoku-checker.py
@@ -4,6 +4,25 @@
  Writing a basci sudoku check
 '''
 
+class Sudoku(object):
+
+    def __init__(self,puzzle):
+        _puzzle = {}
+        for number, row in enumerate(range(9)):
+            _puzzle[row] = puzzle[number]
+        self.puzzle = _puzzle
+
+
+
+    def __str__(self):
+        return "Sudoku puzzle"
+
+    def display(self):
+        for row_num, row in self.puzzle.items():
+            if row_num in [0,3,6] : print("---+---+---")
+            print(f"{i for i in row }")
+
+
 def isuniq(_list):
     '''
     _list: provide a list of elemets and check if all the elemets are uniq
@@ -15,6 +34,8 @@ def isuniq(_list):
         _set.add(element)
     return True
     
+def coloum(puzzle, coloum_num):
+    return [row[coloum_num] for row in puzzle]
 
 test_puzzle = [
 [1,2,3,4,5,6,7,8,9],
@@ -32,5 +53,17 @@ for row in test_puzzle:
     print(f"The row :{row} is {'Valid' if isuniq(row) else 'InValid'}")
 
 
+for _ in range(9):
+    print(f"The coloum : { coloum(test_puzzle, _) } is { 'Valid' if isuniq(coloum(test_puzzle, _)) else 'Invalid' }")
 
+_list=[]
 
+for i in test_puzzle:
+    print(i)
+
+for i in range(4,7):
+    for j in range(4,7):
+        _list.append(test_puzzle[j][i])
+a = Sudoku(test_puzzle)
+a.display()
+print(a)
